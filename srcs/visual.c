97617eb667d76df48fd09547e3b6b624d0d8feed
gopardin <andrey.ilkiv@gmail.com>
2018-09-05 19:12:59 +0300

    first steps

--- srcs/visual.c
+++ srcs/visual.c
@@ -1,22 +1,53 @@
 #include <ncurses.h>
+#include <zconf.h>
 
-void		cycle(void)
+WINDOW	*create_new_win(int width, int height, int start_x, int start_y)
 {
-	
+	WINDOW		*new;
+
+	new = newwin(height, width, start_y, start_x);
+	box(new, 0, 0);
+	wrefresh(new);
+	return (new);
 }
 
-int			main(void)
+void		destroy_win(WINDOW *win)
 {
-	initscr();
-	noecho();
-	printw("hello world!");
-	refresh();
-	char c = getch();
-	printw("%c", c);
-	refresh();
-	cycle();
-	if (getch() == ' ')
-		endwin();
+	wborder(win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+	getch();
+	werase(win);
+	wrefresh(win);
+	delwin(win);
+}
 
+int			main()
+{
+	WINDOW		*win;
+	WINDOW		*temp;
+	int			row;
+	int			col;
+
+	initscr();
+	cbreak();
+	noecho();
+	refresh();
+	win = create_new_win(10, 3, (COLS - 3)/2, (LINES - 10)/2);
+	temp = create_new_win(50, 10, 20, 20);
+
+	getmaxyx(win, row, col);
+	wprintw(win, "lol\n");
+	getmaxyx(temp, row, col);
+	mvwprintw(temp, 10, 10, "kek");
+	wrefresh(win);
+	wrefresh(temp);
+	char c = getch();
+	wprintw(temp, "%c", c);
+	wprintw(win, "%c", c);
+	wrefresh(win);
+	wrefresh(temp);
+	getch();
+	destroy_win(win);
+	getch();
+	endwin();
 	return (0);
 }
